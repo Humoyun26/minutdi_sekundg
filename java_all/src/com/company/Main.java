951e4d01ddf4dcecc1e5ac9fbaa5998fcdcdcc72
Humoyun26 <110013343+Humoyun26@users.noreply.github.com>
2022-07-31 03:01:29 -0400

    Add files via upload

---
+++ java_all/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(in);
+
+
+        System.out.println("Minutdi sekundga: ");
+        int  min = scanner.nextInt();
+        int res =   min * 60;
+        System.out.println(res     +    "  sekund");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
